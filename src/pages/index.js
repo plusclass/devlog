5cfedb1cf2be0dd3149bbec59102119ba692acea
GORAN <miyakawa@plus-class.co.jp>
2021-08-24 20:11:51 +0900

    design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,35 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { Helmet } from "react-helmet";
-
+import styled from "styled-components";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import PostCard from "../components/PostCard";
 import HomeJsonLd from "../components/json/HomeJsonLd";
 
+
+const IndexWrap = styled.div`
+h2 {
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 24px;
+}
+`;
+
+const NewFlex = styled.div`
+max-width: 750px;
+margin: 0 auto;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+`;
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props;
     const siteTitle = data.site.siteMetadata.title;
-    const posts = data.allMarkdownRemark.edges;
+    const postLatest = data.allMarkdownRemark.edges[0];
+    const postsNew = data.allMarkdownRemark.edges.slice(1,6);
+    const postsMore = data.allMarkdownRemark.edges.slice(7);
     const { location } = this.props;
 
     return (
@@ -22,9 +40,20 @@ class BlogIndex extends React.Component {
           <meta property="og:image" content={`https://dev.plus-class.jp/images/ogp.png`} />
         </Helmet>
         <HomeJsonLd />
-        {posts.map(({ node }) => {
-          return <PostCard key={node.fields.slug} node={node} />;
+        <IndexWrap>
+        <h2>最新</h2>
+        <PostCard key={postLatest.node.fields.slug} node={postLatest.node} classProp={`latest`} />
+        <h2>新着</h2>
+        <NewFlex>
+        {postsNew.map(({ node }) => {
+          return <PostCard key={node.fields.slug} node={node} classProp={`new`} />;
         })}
+        </NewFlex>
+        <h2>その他</h2>
+        {postsMore.map(({ node }) => {
+          return <PostCard key={node.fields.slug} node={node} classProp={`more`} />;
+        })}
+        </IndexWrap>
       </Layout>
     );
   }
